04dfbc0fd042ec56dc1c7b0d0fb1e257e62f90dc
septiaprstka <septia.prastika0409@gmail.com>
2023-11-13 15:26:38 +0700

    kategori

---
+++ kategori/input_kategori.php
@@ -0,0 +1,29 @@
+<?php
+include "koneksi.php";
+
+$res = [
+  "status" => 200,
+  "msg" => "",
+  "body" => [
+      "data" => [
+          "nama_kategori" => "",
+      ]
+  ]
+      ];
+$id_kategori = $_POST['id_kategori'];
+$nama_kategori = $_POST['nama_kategori'];
+$q = mysqli_query($conn, "INSERT INTO kategori_tb (id_kategori, nama_kategori) VALUES ('$id_kategori', '$nama_kategori')");
+
+if ($q) {
+
+  $res['status'] = 200;
+  $res['msg'] = "Data berhasil di insert";
+  $res['body']['data']['nama_kategori'] = $nama_kategori;
+} else {
+  $res['status'] = 401;
+  $res['msg'] = "Gagal membuat kategori";
+  $res['body']['error'] = "Kesalahan validasi input";
+}
+
+echo json_encode($res);
+?>
