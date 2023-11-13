4564a4571290f9776230ab9f51aa2c7c93bc8bb2
septiaprstka <septia.prastika0409@gmail.com>
2023-11-14 00:53:48 +0700

    refisi

--- produk/update_produk.php
+++ produk/update_produk.php
@@ -33,11 +33,20 @@ move_uploaded_file($gambar_tmp, $gambar_path);
 $kode_brg = $_GET['kode_brg'];
 $nama_brg = $_POST['nama_brg'];
 $harga_brg = $_POST['harga_brg'];
+$id_kategori = $_POST['id_kategori'];
 $stok_brg = $_POST['stok_brg'];
 $deskripsi_brg = $_POST['deskripsi_brg'];
 
+$i=mysqli_query($conn, "SELECT * FROM kategori_tb");
 
-$q= "UPDATE barang_tb SET gambar_brg='$gambar_brg', nama_brg='$nama_brg',
+while ($row = mysqli_fetch_array($i)) {
+
+   $row['nama_kategori'];
+
+}
+
+
+$q= "UPDATE barang_tb SET gambar_brg='$gambar_brg', nama_brg='$nama_brg', id_kategori='$id_kategori',
 harga_brg='$harga_brg',stok_brg='$stok_brg',deskripsi_brg ='$deskripsi_brg'  WHERE kode_brg='$kode_brg'";
 // $q="INSERT INTO barang_tb (gambar_brg,kode_brg,nama_brg,harga_brg,id_kategori,stok_brg,deskripsi_brg) 
 // VALUES ('$gambar_brg','$kode_brg','$nama_brg','$harga_brg','$id_kategori','$stok_brg','$deskripsi_brg')";
@@ -54,6 +63,7 @@ if ($result) {
   'kode_brg' => $kode_brg,
   'nama_brg' => $nama_brg,
   'harga_brg' => $harga_brg,
+  'id_kategori' => $id_kategori,
   'stok_brg' => $stok_brg,
   'deskripsi_brg' => $deskripsi_brg,
   
